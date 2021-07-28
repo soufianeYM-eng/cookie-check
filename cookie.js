210fad3809e1eee507034eed5d5b5f7880a3e567
Soufiane <yousfisoufiane8@gmail.com>
2021-07-28 17:48:26 +0100

    Add Script

---
+++ cookie.js
@@ -0,0 +1,12 @@
+
+<script>
+    var CookieName = "TestCookie";
+document.cookie = "CookieName=TestCookie;SameSite=None;Secure;";
+
+if (document.cookie.indexOf(CookieName) == -1) {
+    console.log("Cookies are required to use shopping carts.");
+}
+else if (document.cookie.indexOf(CookieName) != -1) {
+    console.log("Thank you for enabling Third-Party cookies");
+}
+</script>
